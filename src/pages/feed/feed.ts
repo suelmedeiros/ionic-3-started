7aa45f8965683b2b091b195ace4bda244463f23c
Maxwell Medeiros <suelso87@hotmail.com>
2017-10-17 01:28:59 -0200

    first commit

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+  public nomeUsuario : string = "Suel Medeiros";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public somaDoisNumeros(num1:number, num2:number): void{
+    alert(num1 + num2);
+  }
+
+  //função do ciclo de vida da página
+  ionViewDidLoad() {
+    //console.log('ionViewDidLoad FeedPage');
+    //this.somaDoisNumeros(5,6);
+  }
+
+}
